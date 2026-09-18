15eeda38982ba5848a3b25f0ae1cfea5355a4519
khk3731-del <khk3731@gmail.com>
2026-09-18 15:05:23 +0900

    Revert "Add web-based claim analytics dashboard"
    This reverts commit 9dfaac308e65da2a9442e925e6a3e6d47f1c380b.

--- run_web_dashboard.py
+++
@@ -1,9 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import webbrowser
-
-ROOT = Path(__file__).resolve().parent
-server = ThreadingHTTPServer(("127.0.0.1", 8765), lambda *args, **kwargs: SimpleHTTPRequestHandler(*args, directory=str(ROOT), **kwargs))
-print("웹 대시보드: http://127.0.0.1:8765/web_dashboard.html")
-webbrowser.open("http://127.0.0.1:8765/web_dashboard.html")
-server.serve_forever()
